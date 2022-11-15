9387f4a68884b631fbceb313123a3338e3f081ab
Thelma-Dev <osifoaisosa@gmail.com>
2022-11-14 22:19:42 -0600

    Animate banner text

--- assets/script/index.js
+++ assets/script/index.js
@@ -18,18 +18,18 @@ const left = select('.fa-chevron-left');
 const right = select('.fa-chevron-right');
 const heroBanner = select('.hero-image');
 
-const images = ['banner-1.jpg', '../img/banner-3.jpg', '../img/banner-4.jpg'];
+const images = ['assets/img/banner-1.jpg', 'assets/img/banner-3.jpg', 'assets/img/banner-4.jpg', 'assets/img/banner-2.jpg'];
 
-// for (let i = 0; i < images.length; i++) {
-//     setTimeout(function() {
-//       let newImg = "url(" + images[i] + ")";
-//       heroBanner.style.backgroundImage = newImg;
+for (let i = 0; i < images.length; i++) {
+    setInterval(function() {
+      let newImg = "url(" + images[i++] + ")";
+      heroBanner.style.backgroundImage = newImg;
   
-//       if (i >= images.length) {
-//         i = 0;
-//       }
-//     }, 3000)
-// }
+      if (i >= images.length) {
+        i = 0;
+      }
+    }, 3000)
+}
 
 
 function sleep(duration) {
@@ -44,3 +44,32 @@ const paragraph = select('.intro-p');
 
 sleep(300).then(() => text.classList.add('is-visible'));
 sleep(300).then(() => paragraph.classList.add('is-visible'));
+
+
+onEvent('mousemove', heroBanner, function() {
+    right.classList.add('is-visible');
+    left.classList.add('is-visible');
+})
+
+
+onEvent('click', right, function() {
+    for (let i = 0; i < images.length; i++) {
+        let newImg = "url(" + images[i++] + ")";
+        heroBanner.style.backgroundImage = newImg;
+
+        if (i == 3) {
+            right.classList.remove('is-visible');
+        }
+    }
+})
+
+onEvent('click', left, function() {
+    for (let i = 0; images.length - 1 >= i; i--) {
+        let newImg = "url(" + images[images.length - 1] + ")";
+        heroBanner.style.backgroundImage = newImg;
+
+        if (i == 0) {
+            left.classList.remove('is-visible');
+        }
+    }
+})
